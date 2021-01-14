a365b2be43a143f6baf8751b6241de1c464bd5a9
Noel1827 <noel.vargas5@upr.edu>
2021-01-14 01:41:13 -0400

    Testings and preparations

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+class HelloWorld{
+    static public void main(String []args){
+        System.out.println("Hello World");
+
+    //  double f = 10.5;
+    //  int a = (int)f;
+    //  System.out.println(f);
+    //  System.out.println(a);
+
+    // int a = 130;
+    // byte b = (byte)a;
+    // System.out.println(a);
+    // System.out.println(b);
+
+    // byte a = 10;
+    // byte b = 100;
+
+    // byte c = (byte)(a+b);
+    // System.out.println(c);
+
+    double a = 10.59;
+    int b = (int)(a);
+    System.out.println(b);
+    
+    }
+}
