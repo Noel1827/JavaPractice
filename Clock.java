94f59c4ac150c125016308ebfa44c8603f12aab6
Noel1827 <noel.vargas5@upr.edu>
2021-01-26 02:25:43 -0400

    ToString, Time and Multiple Constructors using the this() keyWord

--- Clock.java
+++ Clock.java
@@ -1,10 +1,26 @@
 public class Clock {
-    public static void main(String args[]) {
-        TimeClass timeObj = new TimeClass();
+ public static void main(String args[]){
+     MultipleConstructorsClock obj = new MultipleConstructorsClock();
+     MultipleConstructorsClock obj2 = new MultipleConstructorsClock(5);
+     MultipleConstructorsClock obj3 = new MultipleConstructorsClock(5,13);
+     MultipleConstructorsClock obj4 = new MultipleConstructorsClock(5,13,43);
 
-        System.out.println(timeObj.toMilitary());
-
-        timeObj.setTime(13, 27, 6);
-        System.out.println(timeObj.toMilitary());
-    }
+     System.out.printf("%s\n", obj.toMilitary());
+     System.out.printf("%s\n", obj2.toMilitary());
+     System.out.printf("%s\n", obj3.toMilitary());
+     System.out.printf("%s\n", obj4.toMilitary());
+     
+ }
 }
+
+// USSE THIS FOR THE MILITARY, AND NORMAL TIME :)
+   // public static void main(String args[]) {
+    //     TimeClass timeObj = new TimeClass();
+
+    //     System.out.println(timeObj.toMilitary());
+
+    //     timeObj.setTime(13, 27, 6);
+    //     System.out.println(timeObj.toMilitary());
+    //     System.out.println(timeObj.toString());
+        
+    // }
---
+++ ToStringMain.java
@@ -0,0 +1,5 @@
+public class ToStringMain {
+    public static void main(String args[]){
+        ToString obj = new ToString(4,5,6);
+    }
+}
