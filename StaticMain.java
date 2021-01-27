9cdff3c8e7e2e26bde5f7756a0435cb2252a596c
Noel1827 <noel.vargas5@upr.edu>
2021-01-27 01:44:14 -0400

    Static

---
+++ StaticMain.java
@@ -0,0 +1,8 @@
+public class StaticMain {
+    public static void main(String args[]){
+        Static member1 = new Static("Megan", "Fox");
+        Static member2 = new Static("Fox", "Fox");
+        Static member3 = new Static("stupic", "Fox");
+        Static member4 = new Static("Noel", "Fox");
+    }
+}
