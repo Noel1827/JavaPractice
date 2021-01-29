97367e360d76d596baaa26c5d40bf85f684231ef
Noel1827 <noel.vargas5@upr.edu>
2021-01-29 01:15:00 -0400

    Inheritance

---
+++ Inheritance3.java
@@ -0,0 +1,5 @@
+public class Inheritance3 {
+    public void eat(){
+        System.out.println("I am the eat methods");
+    }
+}
---
+++ InheritanceMain.java
@@ -0,0 +1,8 @@
+public class InheritanceMain {
+    public static void main(String args[]){
+        Inheritance1 in1 = new Inheritance1();
+
+        in1.eat();
+       
+    }
+}
