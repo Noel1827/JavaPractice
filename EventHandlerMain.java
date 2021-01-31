ed273792b41f8a8643e3c188715931cd9dc3fce7
Noel1827 <noel.vargas5@upr.edu>
2021-01-31 01:51:10 -0400

    Finished Event Handler

--- EventHandlerMain.java
+++ EventHandlerMain.java
@@ -1,3 +1,13 @@
+import javax.swing.JFrame;
+
 public class EventHandlerMain {
-    
+    public static void main(String args[]){
+
+        EventHandler noel = new EventHandler();
+        noel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        noel.setSize(350,100);
+        noel.setVisible(true);
+
+
+    }
 }
