de7ceb63ddd564e667249c7674fa0e19a09c8dcf
Noel1827 <noel.vargas5@upr.edu>
2021-02-01 01:34:32 -0400

    PolymorphicArguments

--- PolymorphismMain.java
+++ PolymorphismMain.java
@@ -1,12 +1,10 @@
 
 public class PolymorphismMain {
     public static void main(String args[]){
-       Polymorphismkid2 arr[]= new Polymorphismkid2[2];
-       arr[0] = new Polymorphismkid1();
-       arr[1] = new Polymorphismkid3();
-
-        for(int x=0; x<2;x++)
-            arr[x].eat();
+      Polymorphismkid4 noel = new Polymorphismkid4();
+      Polymorphismkid2 father = new Polymorphismkid2();
+      Polymorphismkid1 son = new Polymorphismkid1();
+      noel.digest(son);
         
 
     }
---
+++ Polymorphismkid4.java
@@ -0,0 +1,5 @@
+public class Polymorphismkid4 {
+    public void digest(Polymorphismkid2 x){
+        x.eat();
+    }
+}
