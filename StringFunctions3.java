d527e1780fa4ee0df15d9747fce47164e8cd9d2e
Noel1827 <noel.vargas5@upr.edu>
2021-02-04 00:58:51 -0400

    indexOf function

---
+++ StringFunctions3.java
@@ -0,0 +1,6 @@
+public class StringFunctions3 {
+    public static void main(String args[]){
+        String s = "noelvargasnoelvargasnoelvargas";
+        System.out.println(s.indexOf('l',5));
+    }
+}
