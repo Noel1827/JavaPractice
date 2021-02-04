ab7e53d5517c15d2149af7024c73c7b636fd8405
Noel1827 <noel.vargas5@upr.edu>
2021-02-03 23:32:04 -0400

    Testing

--- PolymorphismMain.java
+++ PolymorphismMain.java
@@ -2,9 +2,10 @@
 public class PolymorphismMain {
     public static void main(String args[]){
       Polymorphismkid4 noel = new Polymorphismkid4();
-      Polymorphismkid2 father = new Polymorphismkid2(); // it gets an error because the class is abstract because that class is only used for inheritance :)
+      Polymorphismkid2 father = new Polymorphismkid3(); // it gets an error because the class is abstract because that class is only used for inheritance :)
       Polymorphismkid1 son = new Polymorphismkid1();
       noel.digest(son);
+      noel.digest(father);
         
 
     }
