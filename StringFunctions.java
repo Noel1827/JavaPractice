cc09592126113ca5ada3e7e352737364a7e18704
Noel1827 <noel.vargas5@upr.edu>
2021-02-04 00:45:10 -0400

    String functions

---
+++ StringFunctions.java
@@ -0,0 +1,8 @@
+public class StringFunctions {
+    public static void main(String args[]){
+        String a = "apples";
+        String b = "bucky";
+        String c = "BUCKY";
+        System.out.println(a.length());
+    }
+}
