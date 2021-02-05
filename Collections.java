d242d4b622c6c0d572e6361d6aae8745a2603f33
Noel1827 <noel.vargas5@upr.edu>
2021-02-04 22:23:52 -0400

    ListsProgram[]

--- Collections.java
+++ Collections.java
@@ -2,6 +2,29 @@ import java.util.*;
 public class Collections {
     public static void main(String args[]){
         String []arr = {"eggs","lasers","hats","pie"};
-        List<String>
+        List<String> list1 = new ArrayList<String>();
+        // add array items to list
+
+        for(String x:arr){
+            list1.add(x);
+        }
+
+        String[] morethings ={"lasers","hats"};
+        List<String> list2 = new ArrayList<String>();
+        
+        for(String y:morethings){
+         list2.add(y);
+        }
+
+        for(int i = 0; i < list1.size(); i++){
+            System.out.printf("%s ", list1.get(i));
+        }
+
+        editlist(list1,list2);
+        System.out.println();
+
+        for(int i = 0; i < list1.size(); i++){
+            System.out.printf("%s ", list1.get(i));
+        }
     }
 }
