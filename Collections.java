1819a3997043134bef73b320816238c5806173b3
Noel1827 <noel.vargas5@upr.edu>
2021-02-04 22:16:02 -0400

    Lists

--- Collections.java
+++ Collections.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Collections {
     public static void main(String args[]){
-        
+        String []arr = {"eggs","lasers","hats","pie"};
+        List<String>
     }
 }
