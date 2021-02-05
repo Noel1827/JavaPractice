9f1074a8c014aed3212e3b341f6e9cfc706b7edf
Noel1827 <noel.vargas5@upr.edu>
2021-02-04 22:40:02 -0400

    collections Program

--- Collections.java
+++ Collections.java
@@ -1,38 +1,71 @@
 import java.util.*;
 public class Collections {
-    public static void main(String args[]){
-        String []arr = {"eggs","lasers","hats","pie"};
-        List<String> list1 = new ArrayList<String>();
-        // add array items to list
+    // public static void main(String args[]){
+    //     String []arr = {"eggs","lasers","hats","pie"};
+    //     List<String> list1 = new ArrayList<String>();
+    //     // add array items to list
 
-        for(String x:arr){
+    //     for(String x:arr){
+    //         list1.add(x);
+    //     }
+
+    //     String[] morethings ={"lasers","hats"};
+    //     List<String> list2 = new ArrayList<String>();
+        
+    //     for(String y:morethings){
+    //      list2.add(y);
+    //     }
+
+    //     for(int i = 0; i < list1.size(); i++){
+    //         System.out.printf("%s ", list1.get(i));
+    //     }
+
+    //     editlist(list1,list2);
+    //     System.out.println();
+
+    //     for(int i = 0; i < list1.size(); i++){
+    //         System.out.printf("%s ", list1.get(i));
+    //     }
+    // }
+    // public static void editlist(Collection<String> l1, Collection<String> l2){
+    //     Iterator<String> it = l1.iterator();
+    //     while(it.hasNext()){
+    //         if(l2.contains(it.next())){
+    //             it.remove();
+    //         }
+    //     }
+    // }
+
+    public static void main(String args[]){
+        String[] items1 = {"hole", "noel", "taysha", "alondra"};
+        String[] items2 = {"noel", "taysha"};
+        List<String> list1 = new ArrayList<String>();
+        List<String> list2 = new ArrayList<String>();
+
+        for(String x:items1){
             list1.add(x);
         }
-
-        String[] morethings ={"lasers","hats"};
-        List<String> list2 = new ArrayList<String>();
-        
-        for(String y:morethings){
-         list2.add(y);
+        for(String x: items2){
+            list2.add(x);
         }
 
-        for(int i = 0; i < list1.size(); i++){
-            System.out.printf("%s ", list1.get(i));
+        for(String x: list1){
+            System.out.printf("%s ", x);
         }
-
-        editlist(list1,list2);
         System.out.println();
+        
+        removeD(list1,list2);
+      for(int i = 0; i < list1.size(); i++){
+          System.out.printf("%s ", list1.get(i));
+      }
 
-        for(int i = 0; i < list1.size(); i++){
-            System.out.printf("%s ", list1.get(i));
-        }
+
     }
-    public static void editlist(Collection<String> l1, Collection<String> l2){
+    public static void removeD(Collection<String> l1, Collection<String> l2){
         Iterator<String> it = l1.iterator();
         while(it.hasNext()){
-            if(l2.contains(it.next())){
-                it.remove();
-            }
+            if(l2.contains(it.next()))
+            it.remove();
         }
     }
 }
