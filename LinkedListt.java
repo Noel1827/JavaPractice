3a379f3167681857c546d8ed5fc1d825a14dbf94
Noel1827 <noel.vargas5@upr.edu>
2021-02-06 01:56:38 -0400

    created the linked list

---
+++ LinkedListt.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.*;
+public class LinkedListt {
+    public static void main(String args[]){
+        String[] things = {"apples", "noobs", "pwnge", "bacon", "goATS"};
+        List<String> list1 = new LinkedList<String>();
+
+    }
+}
