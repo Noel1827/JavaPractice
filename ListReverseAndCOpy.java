81250da185954f750fe6bcb23c1ca636e3a6606d
Noel1827 <noel.vargas5@upr.edu>
2021-02-08 00:12:56 -0400

    FizzBuzz

--- ListReverseAndCOpy.java
+++ ListReverseAndCOpy.java
@@ -15,5 +15,15 @@ public class ListReverseAndCOpy {
         // create a new array and a new list
         Character[] newRay  = new Character[3];
         List<Character> listCopy = Arrays.asList(newRay);
+
+        // copy contents of list int listcopy
+        Collections.copy(listCopy,list1);
+        System.out.println("Copy of list: ");
+        output(listCopy);
+
+        // fill collection with crap
+        Collection.fill(list1,'X');
+        System.out.println("After filling the list: ");
+        output(list1);
     }
 }
